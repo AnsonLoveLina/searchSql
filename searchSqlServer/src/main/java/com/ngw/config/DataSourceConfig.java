9bbbce8cfcf390787ee6a21547aa1414ee3ffe67
zy-xx <xx198742>
2019-10-24 15:30:07 +0800

    commit

--- searchSqlServer/src/main/java/com/ngw/config/DataSourceConfig.java
+++ searchSqlServer/src/main/java/com/ngw/config/DataSourceConfig.java
@@ -12,6 +12,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
